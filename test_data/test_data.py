767e67ff1270b85789595037d1f71f8e95e6c769
wuxiaowei1 <863886199@qq.com>
2022-06-19 14:31:46 +0800

    pytest框架学习

--- test_data/test_data.py
+++ test_data/test_data.py
@@ -2,19 +2,11 @@ import pytest
 from api.base_request import Client
 from tools.read_file import ReadFile
 
-base_url = str(ReadFile.read_config('$.server')["test"])
-class Testcode():
 
-    def test_api(self):
-        params = {
-            "tid": 1,
-            "term_id": 107545,
-            "camp_day": 1,
-            "follow_status": 0
-        }
-        url = base_url + "/smart/msgs/labels"
-        response = Client.baes_request(url, "get", params)
-        assert response["code"] == 0
+def test_main(cases):   # 不使用数据库功能
+    # 发送请求
+    response = Client.action(cases)
 
-if __name__ == '__main__':
-    pytest.main(['-vs']);
+# if __name__ == '__main__':
+#     pytest.main(['-s','test_data.py'])
+
